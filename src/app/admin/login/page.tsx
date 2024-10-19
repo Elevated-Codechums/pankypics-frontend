4e96c03b23c00a9a670e31442ea750bc7a191c43
Maverick7274 <neelanjanmukerji88@gmail.com>
2024-10-20 03:08:27 +0530

    Tanstack Query added

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+import { useQuery, useIsFetching } from "@tanstack/react-query";
+
 export default function Login() {
+	const { data, isLoading, isError, isSuccess } = useQuery({
+		queryKey: ["login"],
+		queryFn: () => {
+			return fetch("http://localhost:4000/").then((res) => res.json());
+		},
+	});
+
 	return (
 		<div>
 			<h1 className="text-3xl font-bold font-raleway">Login page</h1>
+			{isLoading && <p>Loading...</p>}
+			{isError && <p>Error</p>}
+			{isSuccess && <div>{JSON.stringify(data)}</div>}
 		</div>
 	);
 }
