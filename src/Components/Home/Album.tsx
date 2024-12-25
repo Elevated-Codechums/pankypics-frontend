1cfca7ec2b244ecc65c9376562ac181febe2044e
Bhavya <Bhavya@example.com>
2024-12-25 13:05:32 +0530

    framer-motion-applied

--- src/Components/Home/Album.tsx
+++ src/Components/Home/Album.tsx
@@ -1,3 +1,5 @@
+"use client";
+import {motion} from "framer-motion";
 import Image from "next/image"
 
 
@@ -6,37 +8,82 @@ export default function Album(){
         <div className='h-screen flex justify-center items-center bg-backgroundwhite '>
                         <div className='h-[90%] w-[94%] bg-darkbrown rounded-3xl flex '>
                             <h1 className=' justify-center text-7xl ml-[60px] mt-[50px] text-headtext  h-[100px]  font-bold'>Some of My Albums</h1>
-                            <div className='flex flex-row  h-[20rem] mt-[250px]  justify-center items-center  w-[20rem]'>
-                                <div className='text-3xl  ml-[-550px] flex flex-col font-bold text-headtext'>
+                            <div className='flex flex-row  h-[20rem] mt-[250px]  justify-center items-center   w-[20rem]'>
+                                <div className="ml-[-500]bg-white h-[20rem] w-[20rem] rounded-3xl">
                                 
-                                <a href="#">.Portrait</a><br />
-                                <a href='#'>.Landscope</a><br/>
-                                <a href='#'>.Wildlife</a><br/>
-                                <a href='#'>.Candid</a><br/>
+                                    <motion.button 
+                                    whileHover={{scale:1.1}}
+                                    whileTap={{scale:0.9}}
+                                    className='text-3xl  ml-[-550px] flex flex-col font-bold text-headtext'>
+                                       <a href="#">.Portrait<br /></a> 
+                                    </motion.button>
+                                    <motion.button 
+                                    whileHover={{scale:1.1}}
+                                    whileTap={{scale:0.9}}
+                                    className='text-3xl  ml-[-550px] flex flex-col font-bold text-headtext'>
+                                       <a href="#">.Landscape<br /></a> 
+                                    </motion.button>
+                                    <motion.button 
+                                    whileHover={{scale:1.1}}
+                                    whileTap={{scale:0.9}}
+                                    className='text-3xl  ml-[-550px] flex flex-col font-bold text-headtext'>
+                                       <a href="#">.Wildlife<br /></a> 
+                                    </motion.button>
+                                    <motion.button 
+                                    whileHover={{scale:1.1}}
+                                    whileTap={{scale:0.9}}
+                                    className='text-3xl  ml-[-550px] flex flex-col font-bold text-headtext'>
+                                        <a href="#">.Candid<br /></a>
+                                    </motion.button>
+                                    
                                 </div>
-                                <div className='flex gap-4 ml-[100px] flex-row'>
-                                    <Image
+                                
+                                <div className='flex w-130 gap-5 ml-[-300px] flex-row'>
+                                    <motion.div
+                                    whileHover={{
+                                        zoom:1.1,
+                                        cursor: 'pointer'
+                                    }}
+                                    >
+                                    <Image className="rounded-3xl"
                                     
                                     src="https://images.unsplash.com/photo-1733919504972-4486ea3ddc89?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2NXx8fGVufDB8fHx8fA%3D%3D"
-                                    height={60}
-                                    width={300}
+                                    height={70}
+                                    width={1890}
                                     alt=''
                                     
                                     />
-                                    
-                                    <Image
+                                    </motion.div>
+                                    <motion.div
+                                    whileHover={{
+                                        zoom: 1.1,
+                                        cursor: 'pointer'
+                                    }}
+                                    >
+                                    <Image  className="rounded-3xl"
                                     
                                     src="https://images.unsplash.com/photo-1729731322082-c0a7c32229aa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3Mnx8fGVufDB8fHx8fA%3D%3D"
                                     height={60}
-                                    width={300}
+                                    width={1890}
                                     alt=''/>
+                                    </motion.div>
+
+                                    <motion.div
+                                    whileHover={{   
+                                        cursor: 'pointer',  
+                                        zoom: 1.1,}}>
+                                    <Image className="rounded-3xl"
+                                        src="https://images.unsplash.com/photo-1729731322911-2635f53bb7e5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEzfHx8ZW58MHx8fHx8"
+                                        height={100}
+                                        width={1890}
+                                        alt=''/>
+                                    </motion.div>
+
+                                   
                                     
-                                    <Image
-        
-                                    src="https://images.unsplash.com/photo-1729731322911-2635f53bb7e5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEzfHx8ZW58MHx8fHx8"
-                                    height={60}
-                                    width={300}
-                                    alt=''/>
+                                    
+                                    
+                                    
                                     
                                 </div>
                             </div>
--- src/Components/Home/Hero.tsx
+++ src/Components/Home/Hero.tsx
@@ -1,23 +1,41 @@
+"use client";
 import Image from "next/image"
-
+import  {motion} from "framer-motion"
 
 export default function Hero(){
     return(
         
          <div className='h-screen flex justify-center items-center bg-lightbrown '> 
-                        <div className='h-[90%] w-[94%] bg-darkbrown rounded-3xl flex align-center'>
+                        <div className='h-[90%] w-[94%] bg-darkbrown rounded-3xl flex'>
                             <div>
                                 <h1 className=' justify-center text-9xl ml-[60px] mt-[120px] text-headtext   font-bold'>A collection of <br />
-                                    Memories <span className='text-headtext text-base ml-[80px] italic'>photographer's paradise</span></h1>
+                                   <motion.div 
+                                   initial={{opacity:0, }}
+                                    animate={{opacity:1,}}
+                                    transition={{duration:2}}
+
+                                   >
+                                   <span>Memories</span>
+                                    </motion.div>  <span className='text-headtext text-base ml-[80px] italic'>photographer's paradise</span></h1>
                             </div>
                             <div>
-                                <Image className='bottom-20  absolute rounded-3xl  left-20'
-                            
-                                src= "https://images.unsplash.com/photo-1733235014900-380902922aa2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                width = {350}
-                                height = {70}
-                                alt =''
-                                />
+                               <motion.div 
+                               initial={{opacity:0, y: 50}}
+                               animate={{opacity:1, y:0}}
+                               transition={{duration:0.5}}
+                               className='bottom-20  absolute left-20'>
+                                    <Image className="rounded-3xl"
+                                    src= "https://images.unsplash.com/photo-1733235014900-380902922aa2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                    width = {350}
+                                    height = {70}
+                                    alt =''
+                                    />
+                               </motion.div>
+                                
+                                <motion.div 
+                                initial={{opacity:0}}
+                                animate={{opacity:1}}
+                                transition={{duration:0.5}}>
                                 <Image className='top-20 absolute z-10 rounded-3xl right-20'
                                 
                                 src= "https://images.unsplash.com/photo-1732373870219-ac6444949471?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -25,6 +43,8 @@ export default function Hero(){
                                 height = {70}
                                 alt =''
                                 />
+                                </motion.div>
+                                
                                 <h1 className='text-headtext top-[620px] right-40 absolute font-bold text-base ml-[80px] italic z-0'>portraits for one , portraits <br />
                                 for all</h1>
                             </div>
