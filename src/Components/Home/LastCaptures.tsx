aeca80ceb9b63df7c6159c0adef8ec1683bde248
Bhavya <bhavyaaksh9@gmail.com>
2025-02-20 19:30:49 +0530

    changes done

--- src/Components/Home/LastCaptures.tsx
+++ src/Components/Home/LastCaptures.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function LastCaptures() {
     return (
-        <div className="min-h-screen flex justify-center items-center bg-backgroundwhite">
-            <div className="h-[90%] w-[94%] bg-darkbrown rounded-3xl flex flex-col gap-8 p-6 md:p-12">
+        <div className=" flex justify-center items-center bg-backgroundwhite">
+            <div className="h-full w-[94%] bg-darkbrown rounded-3xl flex flex-col gap-8 p-6 md:p-12">
                 <h1 className="text-center text-3xl md:text-5xl lg:text-7xl text-headtext font-bold">
                     My Latest Captures
                 </h1>
-                <div className="flex flex-wrap justify-center gap-4 md:gap-6">
+                <div className="flex flex-wrap h-full justify-center gap-4 md:gap-6">
                     <Image
                         src="https://images.unsplash.com/photo-1618945525824-083e36be3aa2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDY4fHx8ZW58MHx8fHx8"
                         height={250}
